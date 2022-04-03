c54d73c764748fd977c2798fe6301b6aab8fc6f8
Wesley Weisenberger <wesley@berkeley.edu>
2022-04-02 18:41:46 -0700

    add mongodb

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,9 +1,15 @@
-export PORT=2222
 docker pull searxng/searxng
+# port 2222
 # possibly incorporate this into the systemd service
 docker run --restart=always \
-             -d -p ${PORT}:8080 \
+             -d -p 2222:8080 \
              -v "${PWD}/searxng:/etc/searxng" \
-             -e "BASE_URL=http://localhost:$PORT/" \
+             -e "BASE_URL=http://localhost:2222/" \
              -e "INSTANCE_NAME=my-instance" \
              searxng/searxng
+
+docker run --restart=always \
+            --name mongodb -d 
+            -p 27017:27017
+            mongo
+
